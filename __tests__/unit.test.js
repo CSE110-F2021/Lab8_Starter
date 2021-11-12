ed76c32a011ac267a5111e7ef40c08eaecd28a50
sschaeff2023 <sschaeff2023@users.noreply.github.com>
2021-11-11 20:14:17 -0800

    phone numbers

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,18 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('checks first valid phone number', () => {
+    expect(functions.isPhoneNumber(8585871121)).toBe(true);
+});
+
+test('checks second valid phone number', () => {
+    expect(functions.isPhoneNumber(8585342230)).toBe(true);
+});
+
+test('checks first invalid phone number', () => {
+    expect(functions.isPhoneNumber(1234)).toBe(false);
+});
+
+test('checks second invalid phone number', () => {
+    expect(functions.isPhoneNumber(99)).toBe(false);
+});
