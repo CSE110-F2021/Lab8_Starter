92abf12d2ad5d21b616d6317a099b8157d51f7f4
sschaeff2023 <sschaeff2023@users.noreply.github.com>
2021-11-11 19:51:06 -0800

    assets

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
 
-const sum = require('../scripts/sum.js');
+const sum = require('../assets/scripts/sum.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
