3f3c71f84b5d7823d978ff2a0756964fac808548
Atomgroup530 <xul007@ucsd.edu>
2022-11-20 14:23:50 -0800

    isPhoneNumber Test

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,20 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// isPhoneNumber Test
+test('858-319-5053 is a phone number', () => {
+    expect(functions.isPhoneNumber('858-319-5053')).toBe(true);
+});
+
+test('(858)319-5053 is a phone number', () => {
+    expect(functions.isPhoneNumber('(858)319-5053')).toBe(true);
+});
+
+test('858-319-(5053) is NOT a phone number', () => {
+    expect(functions.isPhoneNumber('858-319-(5053)')).toBe(false);
+});
+
+test('858-(319)-5053 is NOT a phone number', () => {
+    expect(functions.isPhoneNumber('8587-(319)-5053')).toBe(false);
+});
+
